433e10ba94b6c4d66bafd59b60adf442c0192afa
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-28 15:02:57 +1000

    styles: add dark-mode

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Background from '../../components/Background/Background';
 import styles from './App.module.css';
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <main className={styles.app}>
-      <Background />
+    <main className={styles.app} data-theme={isDark ? "dark" : "light"}>
+      <div className={styles.bg_wrapper}>
+        <Background />
+      </div>
       <section className={styles.content}>
-        
+        <h1>test</h1>
       </section>
     </main>
   )
