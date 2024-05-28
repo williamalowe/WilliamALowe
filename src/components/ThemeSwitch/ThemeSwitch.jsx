377d9f12994726508aa6d1abb4cbf36ce32eea29
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-28 16:21:25 +1000

    styles: add themeswitcher event motion

--- src/components/ThemeSwitch/ThemeSwitch.jsx
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,11 +1,17 @@
 import styles from './ThemeSwitch.module.css';
+import { motion } from 'framer-motion';
 
 const ThemeSwitch = ({ handleClick }) => {
   return (
-    <button className={styles.switch} onClick={handleClick}>
+    <motion.button 
+      className={styles.switch} 
+      onClick={handleClick}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+    >
       <div></div>
       <div></div>
-    </button>
+    </motion.button>
   )
 }
 
