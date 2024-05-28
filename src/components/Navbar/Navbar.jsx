fe52fe1a2e2d928d87247913af6da43851119eff
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-28 15:43:17 +1000

    styles: add navbar motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,22 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav id="nav">
-      <NavLink>
+      <NavLink to={'/'}>
         <div id="logo">WL</div>
       </NavLink>
       <div className="nav-link-container">
-        <NavLink>About</NavLink>
-        <NavLink>Projects</NavLink>
-        <NavLink>Experience</NavLink>
-        <NavLink>Contact</NavLink>
+        <NavLink to={'/about'}>About
+          <div className="underline"></div>
+        </NavLink>
+        <NavLink to={'/projects'}>Projects
+          <div className="underline"></div>
+        </NavLink>
+        <NavLink to={'/experience'}>Experience
+          <div className="underline"></div>
+        </NavLink>
+        <NavLink to={'/contact'}>Contact
+          <div className="underline"></div>
+        </NavLink>
       </div>
     </nav>
   )
