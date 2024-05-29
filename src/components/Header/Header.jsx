2f3094d9ef369b73f1a61e1fe5a595503f473fd6
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-29 13:01:17 +1000

    style: animate header highlighttext

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,10 @@ const Header = ({ bodyText, highlightText }) => {
   return (
     <div className={styles.header}>
       <h3>{bodyText}</h3>
-      <div className={styles.animated}>
-        <h3>{highlightText}</h3>
+      <div className={styles.animate}>
+        <span hidden="true">{highlightText}</span>
+        <span>{highlightText}</span>
+        <span hidden="true">{highlightText}</span>
       </div>
     </div>
   )
