8149d2df795583c42c57f95928dd81c36eba5cce
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-29 19:40:42 +1000

    style: animate background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,7 +1,21 @@
+import { motion } from "framer-motion";
 import styles from "./Background.module.css";
 
 const Background = () => {
-  return <div className={styles.background}></div>;
-};
+  return (
+    <motion.div 
+    className={styles.background}
+      animate={{ y: '-100vh',
+        x: '-100vw'
+       }}
+      transition={{ duration: 100,
+        repeat: Infinity,
+        repeatType: 'reverse'
 
-export default Background;
+       }}
+    >
+    </motion.div>
+  )
+}
+
+export default Background
