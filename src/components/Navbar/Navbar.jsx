779d8b5a2e1bd951c88d6e45255d53bc9243e9cf
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-29 11:37:28 +1000

    styles: update navbar motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,39 @@
-import './Navbar.css';
+import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <nav id="nav">
-      <NavLink to={'/'}>
-        <div id="logo">WL</div>
+      <NavLink to={"/"}>
+        <motion.div
+          id="logo"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          WL
+        </motion.div>
       </NavLink>
       <div className="nav-link-container">
-        <NavLink to={'/about'}>About
+        <NavLink to={"/about"}>
+          &lt;About&gt;
           <div className="underline"></div>
         </NavLink>
-        <NavLink to={'/projects'}>Projects
+        <NavLink to={"/projects"}>
+          &lt;Projects&gt;
           <div className="underline"></div>
         </NavLink>
-        <NavLink to={'/experience'}>Experience
+        <NavLink to={"/experience"}>
+          &lt;Experience&gt;
           <div className="underline"></div>
         </NavLink>
-        <NavLink to={'/contact'}>Contact
+        <NavLink to={"/contact"}>
+          &lt;Contact&gt;
           <div className="underline"></div>
         </NavLink>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
