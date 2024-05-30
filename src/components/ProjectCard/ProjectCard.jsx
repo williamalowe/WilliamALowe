fc40e6cd210067b1c9a30936c2a06f104203b152
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-30 15:46:01 +1000

    feat: map projects to project-view

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,57 +1,65 @@
-import { useEffect, useRef } from 'react';
-import styles from './ProjectCard.module.css';
+import { useEffect, useRef } from "react";
+import styles from "./ProjectCard.module.css";
 
-const ProjectCard = ({ alignment }) => {
+const ProjectCard = ({
+  alignment,
+  description,
+  stack,
+  iterations,
+  img,
+  repoURL,
+  demoURL,
+}) => {
   const cardRef = useRef(null);
-  
+
   useEffect(() => {
-    if (alignment === 'left') {
-      cardRef.current.style.flexDirection = 'row';
+    if (alignment === "left") {
+      cardRef.current.style.flexDirection = "row";
     } else {
-      cardRef.current.style.flexDirection = 'row-reverse';
+      cardRef.current.style.flexDirection = "row-reverse";
     }
-  }, [])
+  }, []);
   return (
     <div className={styles.card} ref={cardRef}>
       <div className={styles.description}>
         {/* upper */}
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam fugit, voluptatem natus illo molestias delectus nisi. Exercitationem delectus hic, explicabo odio dolores praesentium culpa, doloremque iusto provident voluptates officia nemo.
-        </div>
+        <p>{description}</p>
         {/* lower */}
         <div className={styles.lists}>
           {/* lower-left */}
           <div className={styles.stack}>
             <h5>Technologies</h5>
             <ul>
-              <li>A</li>
-              <li>B</li>
-              <li>C</li>
-              <li>D</li>
+              {stack.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
           </div>
           {/* lower-right */}
           <div className={styles.future}>
             <h5>Future Iterations</h5>
             <ul>
-              <li>A</li>
-              <li>B</li>
-              <li>C</li>
-              <li>D</li>
+              {iterations.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
           </div>
         </div>
         {/* links */}
         <div className={styles.links}>
-          <a href="#">Repo Link</a>
-          <a href="#">Demo Link</a>
+          <a href={repoURL} target="_blank">
+            Repo Link
+          </a>
+          <a href={demoURL} target="_blank">
+            Demo Link
+          </a>
         </div>
       </div>
       <div className={styles.img}>
-        <img src="https://thumbs.dreamstime.com/b/beautiful-woman-lies-wild-flowers-field-reading-book-summer-holiday-rest-education-concept-horizontal-imagee-269649159.jpg" alt="" />
+        <img src={img} alt="project image" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,32 +1,36 @@
-import Accordion from '../../components/Accordion/Accordion';
-import Header from '../../components/Header/Header';
-import ProjectCard from '../../components/ProjectCard/ProjectCard';
-import styles from './Projects.module.css';
+import Accordion from "../../components/Accordion/Accordion";
+import Header from "../../components/Header/Header";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import styles from "./Projects.module.css";
+import projectData from "../../assets/projects.json";
 
 const Projects = () => {
+  const projects = projectData.projects;
+
   return (
     <div className={styles.projects}>
-      <Header 
-        bodyText='Personal'
-        highlightText='Projects.'
-      />
+      <Header bodyText="Personal" highlightText="Projects." />
       <div className={styles.content}>
-        <Accordion 
-          title={'Title Here'}
-          alignment={'left'}
-        >
-          <ProjectCard 
-            alignment={'left'}
-          />
-        </Accordion>
-        <Accordion 
-          title={'Title Here'}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos expedita at quo nam autem, possimus culpa minus aliquid voluptatibus voluptates ipsam ex pariatur alias necessitatibus delectus? Dicta, animi eius? Consectetur?
-        </Accordion>
+        {projects.map((project, index) => (
+          <Accordion
+            key={project.projectName}
+            title={project.projectName}
+            alignment={index % 2 === 0 ? "right" : "left"}
+          >
+            <ProjectCard
+              alignment={index % 2 === 0 ? "right" : "left"}
+              description={project.projectDescription}
+              stack={project.projectStack}
+              iterations={project.projectIterations}
+              img={project.projectImage}
+              repoURL={project.repoURL}
+              demoURL={project.demoURL}
+            />
+          </Accordion>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
