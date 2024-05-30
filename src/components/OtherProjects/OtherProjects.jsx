9f6c0f4340d10fceceb5e7e6138d673aaaf0c8f6
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-30 17:14:20 +1000

    feat: add otherprojects-component

--- src/components/OtherProjects/OtherProjects.jsx
+++ src/components/OtherProjects/OtherProjects.jsx
@@ -1,9 +1,26 @@
 import styles from './OtherProjects.module.css';
+import otherProjectData from "../../assets/otherProjects.json";
 
 const OtherProjects = () => {
+  const projects = otherProjectData.otherProjects;
+
   return (
     <div className={styles.others}>
-      
+      {
+        projects.map((project) => 
+        <article key={project.projectName}>
+          <h5>{project.projectName}</h5>
+          <ul>
+            {
+              project.projectStack.map((item) => 
+              <li key={item}>{item}</li>
+              )
+            }
+          </ul>
+          <p>{project.projectDescription}</p>
+        </article>
+        )
+      }
     </div>
   )
 }
