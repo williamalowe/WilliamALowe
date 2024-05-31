4bce84b3a7fdd2e35fd2acf6782bf01293c8820a
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-31 12:55:48 +1000

    feat: add stepper-component

--- src/views/Experience/Experience.jsx
+++ src/views/Experience/Experience.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import Header from "../../components/Header/Header";
 import styles from "./Experience.module.css";
 import ExperienceSwitch from "../../components/ExperienceSwitch/ExperienceSwitch";
+import professionalData from "../../assets/professionalExp.json";
+import educationData from "../../assets/educationalExp.json";
+import Stepper from "../../components/Stepper/Stepper";
 
 const Experience = () => {
   const [visibleExperience, setVisibleExperience] = useState(true);
+  const professionalList = professionalData.professional;
+  const educationList = educationData.education;
 
   return (
     <div className={styles.experience}>
@@ -14,10 +19,13 @@ const Experience = () => {
         <Header bodyText="Educational" highlightText="Experience" />
       )}
       <div className={styles.content}>
-        <ExperienceSwitch 
-        handleClick={() => setVisibleExperience(!visibleExperience)}
-        visibleTab={visibleExperience}
+        <ExperienceSwitch
+          handleClick={() => setVisibleExperience(!visibleExperience)}
+          visibleTab={visibleExperience}
         />
+        <div className={styles.stepper}>
+          <Stepper listItems={professionalList} />
+        </div>
       </div>
     </div>
   );
