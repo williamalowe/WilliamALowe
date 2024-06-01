05a002a30b398c9054c01c9aac715c8f45a4e30a
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-01 17:57:30 +1000

    styles: add header onmount motion

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,30 @@
+import { motion } from 'framer-motion';
 import styles from './Header.module.css';
 
 const Header = ({ bodyText, highlightText, fontSize }) => {
   return (
-    <div className={styles.header} style={{fontSize: fontSize}}>
+    <motion.div 
+      className={styles.header} 
+      style={{fontSize: fontSize}}
+      initial={{
+        opacity: 0,
+        x: -20
+      }}
+      animate={{
+        opacity: 1,
+        x: 0
+      }}
+      transition={{
+        duration: 0.5
+      }}
+    >
       <h3>{bodyText}</h3>
       <div className={styles.animate}>
         <span hidden={true}>{highlightText}</span>
         <span>{highlightText}</span>
         <span hidden={true}>{highlightText}</span>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
