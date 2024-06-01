2a3e76a23569f175b04ba1f5aaad4caeaef9d88e
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-01 15:26:19 +1000

    styles: minor layout fixes

--- src/components/ContactBanner/ContactBanner.jsx
+++ src/components/ContactBanner/ContactBanner.jsx
@@ -1,30 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Header from '../Header/Header';
-import Socials from '../Socials/Socials';
-import styles from './ContactBanner.module.css';
-import { faEnvelopeOpen, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "../Header/Header";
+import Socials from "../Socials/Socials";
+import styles from "./ContactBanner.module.css";
+import {
+  faEnvelopeOpen,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ContactBanner = () => {
   return (
     <div className={styles.banner}>
-      <p>Looking to get in touch? Feel free to drop me a message regarding <span>anything</span> at all!</p>
-      <Header 
-        bodyText='Currently: '
-        highlightText='Available for work.'
-      />
-      <div className={styles.contactIcons}>
-        <div>
-          <FontAwesomeIcon icon={faLocationDot} />
-          <h5>Melbourne, Australia</h5>
+      <p>
+        Looking to get in touch? Feel free to drop me a message regarding{" "}
+        <span>anything</span> at all!
+      </p>
+      <Header bodyText="Currently: " highlightText="Available for work." />
+      <div className={styles.center}>
+        <div className={styles.contactIcons}>
+          <div>
+            <FontAwesomeIcon icon={faLocationDot} />
+            <h5>Melbourne, Australia</h5>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faEnvelopeOpen} />
+            <h5>WilliamAndrewLowe@Gmail.com</h5>
+          </div>
         </div>
-        <div>
-          <FontAwesomeIcon icon={faEnvelopeOpen} />
-          <h5>WilliamAndrewLowe@Gmail.com</h5>
-        </div>
+        <Socials />
       </div>
-      <Socials />
     </div>
-  )
-}
+  );
+};
 
-export default ContactBanner
+export default ContactBanner;
