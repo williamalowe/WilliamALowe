f6ed58bb7278ea39ce26e032ac0a76f29cb112c6
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-01 16:04:17 +1000

    feat: update carddeck content fields

--- src/components/CardDeck/CardDeck.jsx
+++ src/components/CardDeck/CardDeck.jsx
@@ -1,39 +1,45 @@
-import Card from '../Card/Card';
-import styles from './CardDeck.module.css';
+import Card from "../Card/Card";
+import styles from "./CardDeck.module.css";
 
 const CardDeck = () => {
   return (
     <div className={styles.deck}>
-    <div>
-    <Card 
-          header='Goals'
-        >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque corporis quasi odit nisi molestias dolores libero, vero tenetur ut. Dolorem iusto aut asperiores commodi eaque vero mollitia sit eligendi?</p>
-        </Card>
+      <Card header="Goals">
+        <p>
+          While continually practicing the fundamentals, my next learning goals
+          include transitioning to TypeScript, and diving deeper into Mongo,
+          Express, Node, Jest, and Vitest to ensure I have a robust and
+          versatile skillset. After some time, I also aim to upskill,
+          transitioning from frontend into a full stack developer.
+        </p>
+      </Card>
+      <Card header="Experience">
+        <p>
+          Proficient in HTML, CSS, JavaScript, and React, I am soon to graduate
+          from Deakin University with a Degree in Computer Science. I enjoy the
+          process of learning and enjoy the challenege of tackling new
+          technologies to creatively solve various problems. I also have
+          extensive work experience, having worked in agriculture, hospitality,
+          and social/market research.
+        </p>
+      </Card>
+      <Card header="Interests">
+        <p>
+          I&apos;m a big nerd with a love for TV/movies, gaming, and tech in
+          general. Building things is my jam, including web apps (obviously),
+          PCs and mechanical keyboards, but also enjoy most outdoor activities,
+          especially fishing, camping, longboarding, and swimming.
+        </p>
+      </Card>
+      <Card header="G'day!">
+        <p>
+          My name is William Lowe, a fresh aussie frontend developer, and I want
+          to work with you!
+        </p>
+        <img src="profile.jpg" alt="profile picture" />
+      </Card>
     </div>
-      <div>
-      <Card 
-          header='Experience'
-        >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque corporis quasi odit nisi molestias dolores libero, vero tenetur ut. Dolorem iusto aut asperiores commodi eaque vero mollitia sit eligendi?</p>
-        </Card>
-      </div>
-      <div>
-      <Card 
-          header='Interests'
-        >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque corporis quasi odit nisi molestias dolores libero, vero tenetur ut. Dolorem iusto aut asperiores commodi eaque vero mollitia sit eligendi?</p>
-        </Card>
-      </div>
-      <div>
-        <Card 
-          header='Intro'
-        >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque corporis quasi odit nisi molestias dolores libero, vero tenetur ut. Dolorem iusto aut asperiores commodi eaque vero mollitia sit eligendi?</p>
-        </Card>
-      </div>
-    </div>
-  )
-}
+  );
+};
 
-export default CardDeck
+export default CardDeck;
