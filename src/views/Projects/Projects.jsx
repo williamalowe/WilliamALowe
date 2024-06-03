5f77315791bdce0fadbdcd5d99d6d3c3a0e6b234
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 11:09:03 +1000

    styles: add projects onmount motion

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -4,6 +4,29 @@ import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import styles from "./Projects.module.css";
 import projectData from "../../assets/projects.json";
 import OtherProjects from "../../components/OtherProjects/OtherProjects";
+import { motion } from "framer-motion";
+
+const parent = {
+  hidden: {},
+  mounted: {
+    transition: {
+      delay: 0.5,
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const child = {
+  hidden: {
+    opacity: 0,
+    x: -20
+  },
+  mounted : {
+    opacity: 1,
+    x: 0
+  }
+}
 
 const Projects = () => {
   const projects = projectData.projects;
@@ -11,10 +34,16 @@ const Projects = () => {
   return (
     <div className={styles.projects}>
       <Header bodyText="Personal" highlightText="Projects." fontSize="2.5rem" />
-      <div className={styles.content}>
+      <motion.div className={styles.content}
+        variants={parent}
+        initial='hidden'
+        animate='mounted'
+      >
         {projects.map((project, index) => (
+          <motion.div key={project.projectName}
+            variants={child}
+          >
           <Accordion
-            key={project.projectName}
             title={project.projectName}
             alignment={index % 2 === 0 ? "right" : "left"}
           >
@@ -28,11 +57,16 @@ const Projects = () => {
               demoURL={project.demoURL}
             />
           </Accordion>
+          </motion.div>
         ))}
+        <motion.div
+        variants={child}
+        >
         <Accordion title="Other Projects" alignment="left">
           <OtherProjects />
         </Accordion>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
