036a03e3ef460fc66240fb3755e1fa9e9eacbe44
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 11:09:15 +1000

    styles: add projects onmount motion

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -12,21 +12,21 @@ const parent = {
     transition: {
       delay: 0.5,
       delayChildren: 0.3,
-      staggerChildren: 0.2
-    }
-  }
-}
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const child = {
   hidden: {
     opacity: 0,
-    x: -20
+    x: -20,
   },
-  mounted : {
+  mounted: {
     opacity: 1,
-    x: 0
-  }
-}
+    x: 0,
+  },
+};
 
 const Projects = () => {
   const projects = projectData.projects;
@@ -34,37 +34,34 @@ const Projects = () => {
   return (
     <div className={styles.projects}>
       <Header bodyText="Personal" highlightText="Projects." fontSize="2.5rem" />
-      <motion.div className={styles.content}
+      <motion.div
+        className={styles.content}
         variants={parent}
-        initial='hidden'
-        animate='mounted'
+        initial="hidden"
+        animate="mounted"
       >
         {projects.map((project, index) => (
-          <motion.div key={project.projectName}
-            variants={child}
-          >
-          <Accordion
-            title={project.projectName}
-            alignment={index % 2 === 0 ? "right" : "left"}
-          >
-            <ProjectCard
+          <motion.div key={project.projectName} variants={child}>
+            <Accordion
+              title={project.projectName}
               alignment={index % 2 === 0 ? "right" : "left"}
-              description={project.projectDescription}
-              stack={project.projectStack}
-              iterations={project.projectIterations}
-              img={project.projectImage}
-              repoURL={project.repoURL}
-              demoURL={project.demoURL}
-            />
-          </Accordion>
+            >
+              <ProjectCard
+                alignment={index % 2 === 0 ? "right" : "left"}
+                description={project.projectDescription}
+                stack={project.projectStack}
+                iterations={project.projectIterations}
+                img={project.projectImage}
+                repoURL={project.repoURL}
+                demoURL={project.demoURL}
+              />
+            </Accordion>
           </motion.div>
         ))}
-        <motion.div
-        variants={child}
-        >
-        <Accordion title="Other Projects" alignment="left">
-          <OtherProjects />
-        </Accordion>
+        <motion.div variants={child}>
+          <Accordion title="Other Projects" alignment="left">
+            <OtherProjects />
+          </Accordion>
         </motion.div>
       </motion.div>
     </div>
