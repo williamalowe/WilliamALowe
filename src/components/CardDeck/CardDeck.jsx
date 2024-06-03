433931939e06f1224454661707dc9a39ba9eb4b4
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-06-03 11:31:24 +1000

    fix: correct spelling/content errors

--- src/components/CardDeck/CardDeck.jsx
+++ src/components/CardDeck/CardDeck.jsx
@@ -37,7 +37,7 @@ const CardDeck = () => {
           include transitioning to TypeScript, and diving deeper into Mongo,
           Express, Node, Jest, and Vitest to ensure I have a robust and
           versatile skillset. After some time, I also aim to upskill,
-          transitioning from frontend into a full stack developer.
+          transitioning from frontend into a full stack developement.
         </p>
       </Card>
       </motion.div>
@@ -49,9 +49,9 @@ const CardDeck = () => {
         <p>
           Proficient in HTML, CSS, JavaScript, and React, I am soon to graduate
           from Deakin University with a Degree in Computer Science. I enjoy the
-          process of learning and enjoy the challenege of tackling new
+          process of learning and enjoy the challenge of tackling new
           technologies to creatively solve various problems. I also have
-          extensive work experience, having worked in agriculture, hospitality,
+          a wide-breadth of work experience, having worked in agriculture, hospitality,
           and social/market research.
         </p>
       </Card>
@@ -64,8 +64,9 @@ const CardDeck = () => {
         <p>
           I&apos;m a big nerd with a love for TV/movies, gaming, and tech in
           general. Building things is my jam, including web apps (obviously),
-          PCs and mechanical keyboards, but also enjoy most outdoor activities,
-          especially fishing, camping, longboarding, and swimming.
+          PCs and mechanical keyboards. I also enjoy most outdoor activities, 
+          especially fishing, camping, longboarding, and swimming. If it&apos;s
+          outdoors, theres a good chance that I&apos;ve tried it at least once. 
         </p>
       </Card>
       </motion.div>
@@ -75,7 +76,7 @@ const CardDeck = () => {
 
       <Card header="G'day!">
         <p>
-          My name is William Lowe, a fresh aussie frontend developer, and I want
+          My name is William Lowe, a fresh Aussie frontend developer, and I want
           to work with you!
         </p>
         <img src="profile.jpg" alt="profile picture" />
